623201ee481d2ac2f5a7bc14403ea4a32cd1131c
Albert Smit <talkto@albertsm.it>
2020-10-11 14:17:52 +0200

    Switch development bundler

---
+++ snowpack.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	'plugins': [
+		['@snowpack/plugin-babel'],
+		['@snowpack/plugin-dotenv'],
+		['@snowpack/plugin-webpack'],
+		['@snowpack/plugin-typescript']
+	]
+}
