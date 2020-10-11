1dcb2f7d629db7fae0dd5022abc5168e28584263
Albert Smit <talkto@albertsm.it>
2020-10-11 14:27:47 +0200

    Setting up Snowpack

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,9 @@
 // eslint-disable-next-line no-undef
 module.exports = {
+	'mount': {
+		'public': '/',
+		'src': '/_dist_'
+	},
 	'plugins': [
 		['@snowpack/plugin-babel'],
 		['@snowpack/plugin-dotenv'],
